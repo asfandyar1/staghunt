f134613b9c14952ab2537dd1f5f2043035a79471
Albert Iribarne <airibarne@poblenou-130-43.eduroamcp.upf.edu>
2019-06-27 14:27:40 +0200

    timeit

--- timeit_script.py
+++ timeit_script.py
@@ -1,11 +1,21 @@
 import time
 import random
+import argparse
+import pandas as pd
+import numpy as np
 from torch.cuda import is_available, init
 from staghunt import StagHuntGame, MatrixStagHuntModel, TorchStagHuntModel
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--out', type=str, help='path to the output file')
+parser.add_argument('--lmb', type=float, help='value of lambda')
+args = parser.parse_args()
 
 random.seed(30)  # reproducibility
 
 model = StagHuntGame()
+model.lmb = args.lmb
+model.horizon = 20
 
 loopy = MatrixStagHuntModel()
 matrix = MatrixStagHuntModel()
@@ -14,85 +24,62 @@ if is_available():
     gpu = TorchStagHuntModel(is_cuda=True, var_on=False)
     init()
 
-data = {}
-for n in range(5, 6):
+data = pd.DataFrame(columns=['LMB', 'N', 'M', 'Horizon', 'loopy_time', 'matrix_time', 'cpu_time', 'gpu_time'])
+for n in range(5, 11):
     N = n*n
-    for M in range(2, int(0.8*2*N/7)):  # number of ranges up to ~80% of the grid capacity
-        experiment_data = {}
+    for M in range(2, int(0.5*2*N/7)):  # number of ranges up to ~80% of the grid capacity
 
-        model.new_game_sample(size=(n, n), num_agents=M)
-        conf = model.get_game_config()
-        experiment_data['conf'] = conf
+        for _ in range(5):
+            model.new_game_sample(size=(n, n), num_agents=M)
+            conf = model.get_game_config()
 
-        loopy.set_game_config(conf)
-        matrix.set_game_config(conf)
-        cpu.set_game_config(conf)
-        if is_available():
-            gpu.set_game_config(conf)
+            loopy.set_game_config(conf)
+            matrix.set_game_config(conf)
+            cpu.set_game_config(conf)
+            if is_available():
+                gpu.set_game_config(conf)
 
-        loopy.build_model()
-        matrix.fast_build_model()
-        cpu.fast_build_model()
-        if is_available():
-            gpu.fast_build_model()
+            loopy.build_model()
+            matrix.fast_build_model()
+            cpu.fast_build_model()
+            if is_available():
+                gpu.fast_build_model()
 
-        t0 = time.time()
-        loopy.run_game(inference_type='slow')
-        t1 = time.time()
-        loopy_time = t1 - t0
-        loopy.reset_game()
-
-        t0 = time.time()
-        matrix.run_game(inference_type='matrix')
-        t1 = time.time()
-        matrix_time = t1 - t0
-        matrix.reset_game()
-
-        t0 = time.time()
-        cpu.run_game()
-        t1 = time.time()
-        cpu_time = t1 - t0
-        cpu.reset_game()
-
-        if is_available():
             t0 = time.time()
-            gpu.run_game()
+            loopy.run_game(inference_type='slow')
             t1 = time.time()
-            gpu_time = t1 - t0
-            gpu.reset_game()
+            loopy_time = t1 - t0
+            loopy.reset_game()
 
-        experiment_data['loopy_time'] = loopy_time
-        experiment_data['matrix_time'] = matrix_time
-        experiment_data['cpu_time'] = cpu_time
-        if is_available():
-            experiment_data['gpu_time'] = gpu_time
+            t0 = time.time()
+            matrix.run_game(inference_type='matrix')
+            t1 = time.time()
+            matrix_time = t1 - t0
+            matrix.reset_game()
 
-        data[(N, M)] = experiment_data
+            t0 = time.time()
+            cpu.run_game()
+            t1 = time.time()
+            cpu_time = t1 - t0
+            cpu.reset_game()
 
-print(data)
+            if is_available():
+                t0 = time.time()
+                gpu.run_game()
+                t1 = time.time()
+                gpu_time = t1 - t0
+                gpu.reset_game()
+            else:
+                gpu_time = 0
 
-# n_reps = 5
-# n_instances = 10
-# on_times = []
-# off_times = []
-# for i in range(n_instances):
-#     on_model.new_game_sample(size=(10, 10), num_agents=5)
-#     off_model.set_game_config(on_model.get_game_config())
-#     on_rep_time = 0
-#     off_rep_time = 0
-#     for r in range(n_reps):
-#         on_model.fast_build_model()
-#         off_model.fast_build_model()
-#         t0 = time.time()
-#         on_model.run_game()
-#         t1 = time.time()
-#         off_model.run_game()
-#         t2 = time.time()
-#         on_model.reset_game()
-#         off_model.reset_game()
-#         on_rep_time += t1-t0
-#         off_rep_time += t2-t1
-#     on_times.append(on_rep_time/n_reps)
-#     off_times.append(off_rep_time/n_reps)
-# print(on_times)
-# print(off_times)
+            data = data.append({'LMB': model.lmb,
+                                'N': N,
+                                'M': M,
+                                'Horizon': model.horizon,
+                                'loopy_time': loopy_time,
+                                'matrix_time': matrix_time,
+                                'cpu_time': cpu_time,
+                                'gpu_time': gpu_time}, ignore_index=True)
+
+print("Save results to: " + args.out)
+data.to_pickle(args.out)
