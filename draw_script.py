cba00baafb42eeb651892ef2fd51baec160dd456
Albert Iribarne <airibarne@poblenou-130-43.eduroamcp.upf.edu>
2019-06-27 18:32:56 +0200

    draw

---
+++ draw_script.py
@@ -0,0 +1,18 @@
+import argparse
+import torch as t
+from staghunt import TorchStagHuntModel
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--out', type=str, help='path to the output file')
+parser.add_argument('--size', type=int, help='size of the grid')
+parser.add_argument('--M', type=int, help='number of agents')
+args = parser.parse_args()
+
+cuda = t.cuda.is_available()
+
+model = TorchStagHuntModel(is_cuda=cuda, var_on=False)
+model.new_game_sample(size=(args.size, args.size), num_agents=args.M)
+
+model.fast_build_model()
+model.run_game()
+model.display(file=args.out)
