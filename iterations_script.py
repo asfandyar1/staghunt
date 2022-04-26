c62bf0720b6b500da03425350ef2e57768c15076
asfandyar <asfandyar.abbas01@estudiant.upf.edu>
2022-04-26 16:55:20 +0200

    - Changing path

--- iterations_script.py
+++ iterations_script.py
@@ -7,6 +7,7 @@ from staghunt.mrftools.mrftools import TorchMatrixBeliefPropagator
 from staghunt import TorchStagHuntModel
 import os
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--out', type=str, help='path to the output file')
 parser.add_argument('--size', type=int, help='size of the grid')
@@ -17,26 +18,26 @@ args = parser.parse_args()
 
 cuda = is_available()
 
-model = TorchStagHuntModel()
+model = TorchStagHuntModel(is_cuda=cuda)
 
 df = pd.DataFrame(columns=['lmb', 'num_iter'])
 for i in range(args.niter):
 
     model.new_game_sample(size=(args.size, args.size), num_agents=args.M)
     for lmb in np.arange(1, 101) / 10:
-        with open('C:\\Users\\asfan\\PycharmProjects\\staghunt\\jobs\\iterations.txt', 'w') as f:
+
+        with open('/home/asfandyar/jobs/iterations.txt') as f:
             f.write(str(i) + '-' + str(lmb) + '\t' + str(time.time()) + '\n')
 
         model.lmb = lmb
         model.horizon = args.h
-        model.build_model()
+        model.fast_build_model()
         print(model.lmb)
-        bp = TorchMatrixBeliefPropagator(model.mrf)
+        bp = TorchMatrixBeliefPropagator(model.mrf, is_cuda=cuda, var_on=False)
         bp.set_max_iter(3000)
-        model.mrf.set_max_iter(3000)
-        niter = model.infer(inference_type='matrix', display='full', max_iter=3000)
+        niter = bp.infer(display='none')
         model.reset_game()
-        df_dict = pd.DataFrame({'lmb': lmb, 'num_iter': niter}, index=[0])
-        df = pd.concat([df, df_dict], ignore_index=True)
+        df = df.append({'lmb': lmb,
+                        'num_iter': niter}, ignore_index=True)
 
 df.to_pickle(args.out)
