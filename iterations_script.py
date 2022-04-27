b480faa796b07346656a974f95a8f91075308656
asfandyar <asfandyar.abbas01@estudiant.upf.edu>
2022-04-27 13:26:17 +0200

    - Bug fix

--- iterations_script.py
+++ iterations_script.py
@@ -7,7 +7,6 @@ from staghunt.mrftools.mrftools import TorchMatrixBeliefPropagator
 from staghunt import TorchStagHuntModel
 import os
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--out', type=str, help='path to the output file')
 parser.add_argument('--size', type=int, help='size of the grid')
@@ -25,10 +24,10 @@ for i in range(args.niter):
 
     model.new_game_sample(size=(args.size, args.size), num_agents=args.M)
     for lmb in np.arange(1, 101) / 10:
-
-        with open('C:\\Users\\asfan\\PycharmProjects\\staghunt\\jobs\\iterations.txt', 'w') as f:
+        # with open('C:\\Users\\asfan\\PycharmProjects\\staghunt\\jobs\\iterations.txt', 'a+') as f:
+        with open('/home/asfandyar/jobs/iterations.txt', 'a') as f:
             f.write(str(i) + '-' + str(lmb) + '\t' + str(time.time()) + '\n')
-
+        start_time = time.time()
         model.lmb = lmb
         model.horizon = args.h
         model.build_model()
@@ -38,6 +37,9 @@ for i in range(args.niter):
         niter = bp.infer(display='none')
         model.reset_game()
         df = df.append({'lmb': lmb,
-                        'num_iter': niter}, ignore_index=True)
+                        'num_iter': niter,
+                        'time': time.time() - start_time,
+                        'iter': i,
+                        'lmb': lmb}, ignore_index=True)
 
 df.to_pickle(args.out)
